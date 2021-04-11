1d18952238bff31f0e2579647379c008eb7b3246
Dídac Romero <36265669+DidacRomero@users.noreply.github.com>
2021-04-12 01:07:11 +0200

    Created Shader to print ou model albedo

--- Engine/WorkingDir/shaders.glsl
+++ Engine/WorkingDir/shaders.glsl
@@ -37,6 +37,53 @@ void main()
 #endif
 #endif
 
+#ifdef MODELALBEDO
+
+#if defined(VERTEX)///////////////////////////////////////////////////
+layout(location = 0) in vec3 aPosition;
+//layout(location = 1) in vec3 aNormal;
+layout(location = 2) in vec2 aTexCoord;
+//layout (location = 3) in vec3 aTangent;
+//layout(location = 4) in vec3 aBitangent;
+
+
+out vec2 vTexCoord;
+
+void main()
+{
+	vTexCoord = aTexCoord;
+
+	//We will usually not define the clipping scale manually...
+	//it is usually computed by the projection matrix. Because
+	//we are not passing uniform transforms yet, we increase
+	// the clipping scale so that Patrick fits the screen.
+	float clipingScale = 5.0;
+
+	gl_Position = vec4(aPosition, clippingScale);
+
+	//Patrick looks away from the camera by default, so flip it here.
+	gl_position.z = -gl_Position.z;
+}
+
+
+#elif defined(FRAGMENT)	///////////////////////////////////////////////////////
+
+in vec2 vTexCoord;
+
+uniform sampler2D uTexture;
+
+layout(location = 0) out vec4 oColor;
+
+void main()
+{
+oColor = texture(uTexture,vTexCoord);
+}
+
+#endif
+#endif
+
+
+
 
 // NOTE: You can write several shaders in the same file if you want as
 // long as you embrace them within an #ifdef block (as you can see above).
